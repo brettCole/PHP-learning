165dd1f20ee4ded20b43aed51801676bb1b8580d
github username <github email address>
2018-02-26 20:26:49 -0500

    php learning variables

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// Variables
+$name = "Brett";
+echo "<p>My name is $name</p>";
+
+$string1 = "<p>This is the first string.";
+$string2 = "This is the second string.</p>";
+echo $string1." ".$string2;
+
+$myNumber = 45;
+$calculation = $myNumber * 31 / 97 + 4;
+echo "The result of the calculation is ".$calculation;
+
+$myBool = true;
+echo "<p>This statement is true? ".$myBool."</p>";
+
+$variableName = "name";
+echo $$variableName;
+
+?>
